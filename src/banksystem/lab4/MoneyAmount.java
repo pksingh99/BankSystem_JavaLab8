2210dff06f266db250894f32c215e773b32288b7
Sweider.b <sweider.b@gmail.com>
2014-05-10 16:59:07 +0300

    Money amount

---
+++ src/banksystem/lab4/MoneyAmount.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package banksystem.lab4;
+
+/**
+ *
+ * @author alex
+ */
+public class MoneyAmount {
+    final int dollars;
+
+    public MoneyAmount(int value) {
+        this.dollars = value;
+    }
+    
+    public int getValue(){
+        return this.dollars;
+    }
+    
+    public MoneyAmount substract(MoneyAmount substructingMoney){
+        int newValue = this.getValue() - substructingMoney.getValue();
+        MoneyAmount result = new MoneyAmount(newValue);
+        return result;
+    }
+    
+    public MoneyAmount add(MoneyAmount addingMoney){
+        int newValue = this.getValue() + addingMoney.getValue();
+        MoneyAmount result = new MoneyAmount(newValue);
+        return result;
+    }
+}
